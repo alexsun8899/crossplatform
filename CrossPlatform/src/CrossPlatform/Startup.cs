9648d65ba3cdf464f67c68822eb04c77a0f71a69
Alex Sun <alexsun8866@gmail.com>
2016-08-21 17:27:59 +1000

    added a comment

--- CrossPlatform/src/CrossPlatform/Startup.cs
+++ CrossPlatform/src/CrossPlatform/Startup.cs
@@ -8,6 +8,9 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
+/// <summary>
+/// 
+/// </summary>
 namespace CrossPlatform
 {
     public class Startup
